93eccfd2c31c75df10fa762991ecd68b69c7c069
HK-1104 <hkoripalli11@gmail.com>
2021-09-20 14:34:26 +0530

    test class added

---
+++ sampleapp/src/main/java/org/capg/demo/TestClass.java
@@ -0,0 +1,12 @@
+package org.capg.demo;
+
+public class TestClass {
+	
+	public void test() {
+		System.out.println("Test class validated");
+		System.out.println("All test functionalities goes here");
+		System.out.println("Test class validated");
+		System.out.println("All test functionalities goes here");
+	}
+
+}
